baf32322662cf9794cb278e976819a000c4ca154
D1ffic00lt <dm.filinov@gmail.com>
2024-01-23 00:40:55 +0700

    added errors halnder in the genertor api

--- generator/api.py
+++ generator/api.py
@@ -18,29 +18,38 @@ gen.TEMPLATE["modelUri"] = model_uri
 @app.route('/generator/api/v1/catastrophe', methods=['POST'])
 async def generate_catastrophe():
     cat = {}
+    limit = 10
     while cat == {}:
         try:
             cat = await gen.generate_catastrophe()
         except (KeyError, json.decoder.JSONDecodeError):
+            limit -= 1
             cat = {}
+    if limit == 0:
+        return make_response(jsonify({"status": False}), 501)
     return make_response(jsonify(cat), 201)
 
 
 @app.route('/generator/api/v1/bunker', methods=['POST'])
 async def generate_bunker():
     bunker = {}
+    limit = 10
     while bunker == {}:
         try:
             bunker = await gen.generate_bunker()
         except (KeyError, json.decoder.JSONDecodeError):
+            limit -= 1
             bunker = {}
+    if limit == 0:
+        return make_response(jsonify({"status": False}), 501)
     return make_response(jsonify(bunker), 201)
 
 
 @app.route('/generator/api/v1/player/<game_code>', methods=['POST'])
 async def generate_player(game_code):
     user_data = {}
-    while user_data == {}:
+    limit = 10
+    while user_data == {} or limit == 0:
         try:
             user_data = await gen.generate_player(game_code)
             if list(user_data.keys()) != [
@@ -49,13 +58,18 @@ async def generate_player(game_code):
             ]:
                 user_data = {}
         except (KeyError, json.decoder.JSONDecodeError):
+            limit -= 1
             user_data = {}
+    if limit == 0:
+        return make_response(jsonify({"status": False}), 501)
+    gen.games[game_code].add_user_data(user_data)
     return make_response(jsonify(user_data), 201)
 
 
 @app.route('/generator/api/v1/bunker-result', methods=['POST'])
 async def generate_bunker_result():
     result = {}
+    limit = 10
     while result == {}:
         try:
             result = await gen.generate_bunker_result(request.json)
@@ -63,17 +77,23 @@ async def generate_bunker_result():
                 result = {}
         except (KeyError, json.decoder.JSONDecodeError):
             result = {}
+    if limit == 0:
+        return make_response(jsonify({"status": False}), 501)
     return make_response(jsonify(result), 201)
 
 
 @app.route('/generator/api/v1/result', methods=['POST'])
 async def generate_result():
     result = {}
+    limit = 10
     while result == {}:
         try:
             result = await gen.generate_result(request.json)
             if list(result.keys()) != ["result"]:
                 result = {}
         except (KeyError, json.decoder.JSONDecodeError):
+            limit -= 1
             result = {}
+    if limit == 0:
+        return make_response(jsonify({"status": False}), 501)
     return make_response(jsonify(result), 201)
