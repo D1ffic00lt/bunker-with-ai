0def9eebbf0d0353be8ce8f2448740fbb38f12bc
D1ffic00lt <dm.filinov@gmail.com>
2024-05-12 13:41:19 +0700

    add votes streaming

--- info-streaming/app.py
+++ info-streaming/app.py
@@ -18,7 +18,8 @@ not_found_frame_data = {
     "fact1_revealed": False,
     "fact2_revealed": False,
     "phobia_revealed": False,
-    "hobby_revealed": False
+    "hobby_revealed": False,
+    "number_of_votes": 0
 }
 
 
@@ -108,12 +109,12 @@ async def get_user_frame(host_id, user_id):
 
         game_code = await requests.get(f"http://api:9462/bunker/api/v1/game-code/{host_id}")
         if game_code.status_code // 100 in [4, 5]:
-            return render_template("frame.html", image_base64=await get_not_found_frame())
+            return render_template("frame.html", image_base64=(await get_not_found_frame()))
 
         game_code = game_code.json()["game_code"]
         game = await requests.get("http://api:9462/bunker/api/v1/get-game/{}".format(game_code), timeout=60)
         if game.status_code // 100 in [4, 5]:
-            return render_template("frame.html", image_base64=await get_not_found_frame())
+            return render_template("frame.html", image_base64=(await get_not_found_frame()))
 
         game = game.json()
 
@@ -127,7 +128,8 @@ async def get_user_frame(host_id, user_id):
             return make_response(jsonify({"status": False}), 404)
         user_code = (f"{user_data['gender_revealed']}{user_data['health_revealed']}{user_data['profession_revealed']}"
                      f"{user_data['hobby_revealed']}{user_data['luggage_revealed']}{user_data['age_revealed']}"
-                     f"{user_data['fact1_revealed']}{user_data['fact2_revealed']}{user_data['phobia_revealed']}")
+                     f"{user_data['fact1_revealed']}{user_data['fact2_revealed']}{user_data['phobia_revealed']}"
+                     f"{user_data['number_of_votes']}")
 
         if redis_db.get(f"{host_id}:{user_id}") == user_code and redis_db.exists(f"frame_{host_id}:{user_id}"):
             frame = redis_db.get(f"frame_{host_id}:{user_id}")
