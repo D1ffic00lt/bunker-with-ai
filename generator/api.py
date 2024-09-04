f2133c4f1d93603765bedca1f36d4c9db3493396
D1ffic00lt <dm.filinov@gmail.com>
2024-09-04 16:35:33 +0300

    added error messages return

--- generator/api.py
+++ generator/api.py
@@ -1,15 +1,14 @@
 import json
 import os
-
 import asyncio
-
-import config
 
 from flask import Flask, make_response, jsonify, request
 
+import config
 from generator import Generator
 
 app = Flask(__name__)
+
 if os.environ.get('AUTH_TYPE') == "iam":
     with open(os.environ.get('TOKEN')) as token_file:
         token = token_file.read().strip()
@@ -25,39 +24,47 @@ gen.TEMPLATE["modelUri"] = model_uri
 
 @app.route('/generator/api/v1/catastrophe', methods=['POST'])
 async def generate_catastrophe():
-    cat = {}
+    catastrophe = {}
+    error_message = ""
     limit = 4
-    while cat == {}:
+
+    while catastrophe == {}:
         try:
-            cat = await gen.generate_catastrophe()
-        except (KeyError, json.decoder.JSONDecodeError):
+            catastrophe = await gen.generate_catastrophe()
+        except (KeyError, json.decoder.JSONDecodeError) as e:
+            if isinstance(e, KeyError):
+                error_message = e.args[0]
             limit -= 1
             await asyncio.sleep(1)
-            cat = {}
+            catastrophe = {}
         if limit == 0:
-            return make_response(jsonify({"status": False}), 502)
-    return make_response(jsonify(cat), 201)
+            return make_response(jsonify({"status": False, "error": error_message}), 502)
+    return make_response(jsonify(catastrophe), 201)
 
 
 @app.route('/generator/api/v1/bunker', methods=['POST'])
 async def generate_bunker():
     bunker = {}
+    error_message = ""
     limit = 4
     while bunker == {}:
         try:
             bunker = await gen.generate_bunker()
-        except (KeyError, json.decoder.JSONDecodeError):
+        except (KeyError, json.decoder.JSONDecodeError) as e:
+            if isinstance(e, KeyError):
+                error_message = e.args[0]
             limit -= 1
             await asyncio.sleep(1)
             bunker = {}
         if limit == 0:
-            return make_response(jsonify({"status": False}), 502)
+            return make_response(jsonify({"status": False, "error_message": error_message}), 502)
     return make_response(jsonify(bunker), 201)
 
 
 @app.route('/generator/api/v1/player/<game_code>', methods=['POST'])
 async def generate_player(game_code):
     user_data = {}
+    error_message = ""
     limit = 4
     while user_data == {} or limit == 0:
         try:
@@ -70,11 +77,13 @@ async def generate_player(game_code):
                 limit -= 1
                 await asyncio.sleep(1)
         except (KeyError, json.decoder.JSONDecodeError, IndexError) as e:
+            if isinstance(e, KeyError):
+                error_message = e.args[0]
             limit -= 1
             await asyncio.sleep(1)
             user_data = {}
         if limit == 0:
-            return make_response(jsonify({"status": False}), 502)
+            return make_response(jsonify({"status": False, "error": error_message}), 502)
     gen.games[game_code].add_user_data(user_data)
     return make_response(jsonify(user_data), 201)
 
@@ -82,6 +91,7 @@ async def generate_player(game_code):
 @app.route('/generator/api/v1/bunker-result', methods=['POST'])
 async def generate_bunker_result():
     result = {}
+    error_message = ""
     limit = 4
     while result == {}:
         try:
@@ -90,12 +100,14 @@ async def generate_bunker_result():
                 result = {}
                 limit -= 1
                 await asyncio.sleep(1)
-        except (KeyError, json.decoder.JSONDecodeError):
+        except (KeyError, json.decoder.JSONDecodeError) as e:
+            if isinstance(e, KeyError):
+                error_message = e.args[0]
             result = {}
             limit -= 1
             await asyncio.sleep(1)
     if limit == 0:
-        return make_response(jsonify({"status": False}), 502)
+        return make_response(jsonify({"status": False, "error": error_message}), 502)
     return make_response(jsonify(result), 201)
 
 
@@ -110,6 +122,7 @@ async def get_active_card(card):
 @app.route('/generator/api/v1/result', methods=['POST'])
 async def generate_result():
     result = {}
+    error_message = ""
     limit = 4
     while result == {}:
         try:
@@ -117,10 +130,12 @@ async def generate_result():
             if list(result.keys()) != ["result"]:
                 result = {}
                 limit -= 1
-        except (KeyError, json.decoder.JSONDecodeError):
+        except (KeyError, json.decoder.JSONDecodeError) as e:
+            if isinstance(e, KeyError):
+                error_message = e.args[0]
             limit -= 1
             result = {}
             await asyncio.sleep(1)
         if limit == 0:
-            return make_response(jsonify({"status": False}), 502)
+            return make_response(jsonify({"status": False, "error": error_message}), 502)
     return make_response(jsonify(result), 201)
